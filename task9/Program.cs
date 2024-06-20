b29c87d06db64d7fe5efc8b769f218c53fff0c3f
Administrator <Administrator@WIN-S09DU0P86JK>
2024-06-20 16:47:33 +0300

    Добавьте файлы проекта.

---
+++ task9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 2, 5, 8, 11, 14, 17, 20, 23 };
+
+        Console.WriteLine("Нечетные числа из массива:");
+        foreach (int number in numbers)
+        {
+            if (number % 2 != 0)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
